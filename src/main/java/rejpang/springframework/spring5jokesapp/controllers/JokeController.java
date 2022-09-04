7f02f872d6c33a70d1ef4e6544302d62eb885304
rejpang <rejpang@gmail.com>
2022-09-04 15:21:13 +0800

    Create MVC Controller Closes #5

--- src/main/java/rejpang/springframework/spring5jokesapp/controllers/JokeController.java
+++ src/main/java/rejpang/springframework/spring5jokesapp/controllers/JokeController.java
@@ -1,2 +1,23 @@
-package rejpang.springframework.spring5jokesapp.controllers;public class JokeController {
+package rejpang.springframework.spring5jokesapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import rejpang.springframework.spring5jokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
 }
